283f6a9bcf90309b56f0f8e327305084d1f8552b
unknown <hemakumarm72@gmail.com>
2021-08-21 18:15:14 +0530

    files structure finished

--- src/Components/pageLayout/header/header.jsx
+++ src/Components/pageLayout/header/header.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Header() {
-    return <h1>header</h1>;
+    return (
+        <>
+            <h1>fsdf</h1>
+        </>
+    );
 }
 
 export default Header;
