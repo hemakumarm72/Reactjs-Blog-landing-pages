4fb97d404e14923bcf04eeb249f2a0b4e6320caf
unknown <hemakumarm72@gmail.com>
2021-08-22 09:51:12 +0530

    lazy loader add

--- src/Components/pageLayout/header/Header.jsx
+++ src/Components/pageLayout/header/Header.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import 'Styles/Header/Header.css';
-import Headeritem from 'Components/pageLayout/header/header_item/Header_item';
+// import Headeritem from 'Components/pageLayout/header/header_item/Header_item';
+
+const Headeritem = lazy(() =>
+    import('Components/pageLayout/header/header_item/Header_item')
+);
 
 function Header() {
     return (
